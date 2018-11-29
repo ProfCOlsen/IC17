ca5ce6409e840ffdd7ca879231ce0b10d1f55073
Samuel Vaccaro <scvaccar@sundevils.asu.edu>
2018-11-29 14:59:23 -0700

    Sam Vaccaro added a button.

--- IC17/Form1.Designer.cs
+++ IC17/Form1.Designer.cs
@@ -28,111 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.referenceLbl = new System.Windows.Forms.Label();
-            this.jKellyButton = new System.Windows.Forms.Button();
-            this.chadHayesButton = new System.Windows.Forms.Button();
-            this.raajpaalGohlwarButton = new System.Windows.Forms.Button();
-            this.HaydenBurnsButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.jessicaSheldonButton = new System.Windows.Forms.Button();
+            this.samVaccaroButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // referenceLbl
+            // samVaccaroButton
             // 
-            this.referenceLbl.AutoSize = true;
-            this.referenceLbl.Location = new System.Drawing.Point(567, 648);
-            this.referenceLbl.Name = "referenceLbl";
-            this.referenceLbl.Size = new System.Drawing.Size(132, 20);
-            this.referenceLbl.TabIndex = 0;
-            this.referenceLbl.Text = "Front of the room";
-            // 
-            // jKellyButton
-            // 
-            this.jKellyButton.Location = new System.Drawing.Point(480, 258);
-            this.jKellyButton.Name = "jKellyButton";
-            this.jKellyButton.Size = new System.Drawing.Size(71, 61);
-            this.jKellyButton.TabIndex = 1;
-            this.jKellyButton.Text = "John Kelly";
-            this.jKellyButton.UseVisualStyleBackColor = true;
-            this.jKellyButton.Click += new System.EventHandler(this.jKellyButton_Click);
-            // 
-            // chadHayesButton
-            // 
-            this.chadHayesButton.Location = new System.Drawing.Point(371, 258);
-            this.chadHayesButton.Name = "chadHayesButton";
-            this.chadHayesButton.Size = new System.Drawing.Size(74, 64);
-            this.chadHayesButton.TabIndex = 1;
-            this.chadHayesButton.Text = "Chad Hayes";
-            this.chadHayesButton.UseVisualStyleBackColor = true;
-            this.chadHayesButton.Click += new System.EventHandler(this.chadHayesButton_Click);
-            // 
-            // raajpaalGohlwarButton
-            // 
-            this.raajpaalGohlwarButton.Location = new System.Drawing.Point(236, 262);
-            this.raajpaalGohlwarButton.Name = "raajpaalGohlwarButton";
-            this.raajpaalGohlwarButton.Size = new System.Drawing.Size(91, 57);
-            this.raajpaalGohlwarButton.TabIndex = 0;
-            this.raajpaalGohlwarButton.Text = "Raajpaal Gohlwar";
-            this.raajpaalGohlwarButton.Click += new System.EventHandler(this.raajpaalGohlwarButton_Click);
-            // 
-            // HaydenBurnsButton
-            // 
-            this.HaydenBurnsButton.Location = new System.Drawing.Point(698, 20);
-            this.HaydenBurnsButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.HaydenBurnsButton.Name = "HaydenBurnsButton";
-            this.HaydenBurnsButton.Size = new System.Drawing.Size(110, 37);
-            this.HaydenBurnsButton.TabIndex = 2;
-            this.HaydenBurnsButton.Text = "Hayden";
-            this.HaydenBurnsButton.UseVisualStyleBackColor = true;
-            this.HaydenBurnsButton.Click += new System.EventHandler(this.HaydenBurnsButton_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(367, 405);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(135, 20);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Front of the Class";
-            // 
-            // jessicaSheldonButton
-            // 
-            this.jessicaSheldonButton.Location = new System.Drawing.Point(684, 258);
-            this.jessicaSheldonButton.Name = "jessicaSheldonButton";
-            this.jessicaSheldonButton.Size = new System.Drawing.Size(89, 61);
-            this.jessicaSheldonButton.TabIndex = 4;
-            this.jessicaSheldonButton.Text = "Jessica Sheldon";
-            this.jessicaSheldonButton.UseVisualStyleBackColor = true;
-            this.jessicaSheldonButton.Click += new System.EventHandler(this.jessicaSheldonButton_Click);
+            this.samVaccaroButton.Location = new System.Drawing.Point(79, 163);
+            this.samVaccaroButton.Name = "samVaccaroButton";
+            this.samVaccaroButton.Size = new System.Drawing.Size(88, 23);
+            this.samVaccaroButton.TabIndex = 0;
+            this.samVaccaroButton.Text = "Sam Vaccaro";
+            this.samVaccaroButton.UseVisualStyleBackColor = true;
+            this.samVaccaroButton.Click += new System.EventHandler(this.samVaccaroButton_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 443);
-            this.Controls.Add(this.jessicaSheldonButton);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.HaydenBurnsButton);
-            this.Controls.Add(this.raajpaalGohlwarButton);
-            this.Controls.Add(this.chadHayesButton);
-            this.Controls.Add(this.jKellyButton);
-            this.Controls.Add(this.referenceLbl);
+            this.ClientSize = new System.Drawing.Size(833, 442);
+            this.Controls.Add(this.samVaccaroButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Launch Page - Bios";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label referenceLbl;
-        private System.Windows.Forms.Button jKellyButton;
-        private System.Windows.Forms.Button chadHayesButton;
-        private System.Windows.Forms.Button raajpaalGohlwarButton;
-        private System.Windows.Forms.Button HaydenBurnsButton;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button jessicaSheldonButton;
+        private System.Windows.Forms.Button samVaccaroButton;
     }
 }
 
--- IC17/Form1.cs
+++ IC17/Form1.cs
@@ -15,33 +15,11 @@ namespace IC17
         public Form1()
         {
             InitializeComponent();
-            int y = 0;
-            int x = 0;
         }
 
-        private void chadHayesButton_Click(object sender, EventArgs e)
+        private void samVaccaroButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("This is Chad Hayes Button Bio");
-        }
-
-        private void raajpaalGohlwarButton_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("This is Raaj Paal Button Bio");
-        }
-
-        private void jKellyButton_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("This is John Kelly's Button Bio");
-        }
-
-        private void HaydenBurnsButton_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("This is Hayden Burns's Bio.");
-        }
-
-        private void jessicaSheldonButton_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("This is Jessica Sheldon's bio:");
+            MessageBox.Show("This is Sam Vaccaro's Button!");
         }
     }
 }
